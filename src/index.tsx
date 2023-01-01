08d215bbc6359e49d6e6f2e90aa33c39257860d9
pphelpme <98599731+pphelpme@users.noreply.github.com>
2023-01-01 19:58:29 +0800

    refactor: move methods to index

--- src/index.tsx
+++ src/index.tsx
@@ -1,1 +1,75 @@
-export * from './cvaTools';
+import { cva } from 'class-variance-authority';
+import { ClassProp } from 'class-variance-authority/dist/types';
+import React, { ComponentPropsWithoutRef, FC } from 'react';
+import { CvaParameters, CvaReturnType, WithCvaVariant } from './types';
+
+/**
+ * add VariantsProps of [customCva] to [component]
+ */
+export const withCva = <
+  VariantsType,
+  ComponentType extends React.ElementType,
+  RefType,
+  CvaType extends CvaReturnType<VariantsType> = CvaReturnType<VariantsType>
+>(
+  component: ComponentType,
+  customCva: CvaType
+) => {
+  return React.forwardRef<
+    RefType,
+    WithCvaVariant<CvaType> &
+      Omit<
+        ComponentPropsWithoutRef<ComponentType>,
+        keyof WithCvaVariant<CvaType>
+      >
+  >((props, ref) => {
+    const Component: React.ElementType | JSX.IntrinsicElements = component;
+    const { variants, ...componentProps } = props;
+    const cvaParams: any = {
+      ...variants,
+      class: componentProps.className,
+    };
+    return (
+      <Component
+        {...componentProps}
+        className={customCva(cvaParams)}
+        ref={ref}
+      />
+    );
+  });
+};
+
+export const withVariants = <
+  VariantsType,
+  ComponentType extends React.ElementType,
+  RefType
+>(
+  component: ComponentType,
+  ...cvaArgs: CvaParameters<VariantsType>
+) => {
+  const customCva = cva<VariantsType>(...cvaArgs);
+  return withCva<VariantsType, ComponentType, RefType>(component, customCva);
+};
+
+/**
+ * add default variants to [component]
+ */
+export const withDefaultVariants = <
+  ComponentType extends FC<WithCvaVariant>,
+  RefType
+>(
+  component: ComponentType,
+  defaultVariants: ComponentPropsWithoutRef<ComponentType>['variants'] &
+    ClassProp
+) => {
+  return React.forwardRef<RefType, ComponentPropsWithoutRef<ComponentType>>(
+    (props, ref) => {
+      const variants = {
+        ...defaultVariants,
+        ...props.variants,
+      };
+      const Component: React.ElementType | JSX.IntrinsicElements = component;
+      return <Component ref={ref} {...props} variants={variants} />;
+    }
+  );
+};
---
+++ src/types.ts
@@ -0,0 +1,13 @@
+import type { VariantProps, cva as _cva } from 'class-variance-authority';
+
+// ref: https://github.com/microsoft/TypeScript/issues/42873
+// The inferred type of "X" cannot be named without a reference to "Y". This is likely not portable. A type annotation is necessary.
+export type _VariantProps<T extends (...args: any) => any> = VariantProps<T>;
+export type CvaReturnType<VariantsType> = ReturnType<typeof _cva<VariantsType>>;
+export type CvaParameters<VariantsType> = Parameters<typeof _cva<VariantsType>>;
+
+export type WithCvaVariant<
+  T extends (...args: any) => any = (...args: any) => any
+> = {
+  variants?: _VariantProps<T>;
+};
